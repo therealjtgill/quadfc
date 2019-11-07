387be85a0e47252d596ce6fa883197389b39e92e
therealjtgill <jtgill@email.arizona.edu>
2019-11-07 14:07:33 -0700

    Started updating complementary filter *.ino file with trapezoidal integration of gyro measurements and sin(omega_z*dt) modification to roll and pitch.

--- mpu6050compfilt/mpu6050compfilt.ino
+++ mpu6050compfilt/mpu6050compfilt.ino
@@ -123,6 +123,9 @@ void loop(void)
   static float phi_gyr_rad = 0.;
   static float theta_gyr_rad = 0.;
 
+  static float phi_prev_degps = 0.;
+  static float theta_prev_degps = 0.;
+
   static bool initialized = false;
 
   // Calibrate the gyro by collecting a series of measurements
@@ -137,56 +140,70 @@ void loop(void)
   dt = (micros() - t)/1e6;
   t = micros();
   getImuData(acc, gyro);
-  gyro_rad[0] = (0.7*gyro_rad[0] + 0.3*(gyro[0] - phi_gyr_bias))*M_PI/180.;
-  gyro_rad[1] = (0.7*gyro_rad[1] + 0.3*(gyro[1] - theta_gyr_bias))*M_PI/180.;
-  gyro_rad[2] = (0.7*gyro_rad[2] + 0.3*(gyro[2] - psi_gyr_bias))*M_PI/180.;
+//  gyro_rad[0] = (0.7*gyro_rad[0] + 0.3*(gyro[0] - phi_gyr_bias))*M_PI/180.;
+//  gyro_rad[1] = (0.7*gyro_rad[1] + 0.3*(gyro[1] - theta_gyr_bias))*M_PI/180.;
+//  gyro_rad[2] = (0.7*gyro_rad[2] + 0.3*(gyro[2] - psi_gyr_bias))*M_PI/180.;
+//
+//  phi_rad = phi*M_PI/180.;
+//  theta_rad = theta*M_PI/180.;
 
-  phi_rad = phi*M_PI/180.;
-  theta_rad = theta*M_PI/180.;
-/*
-  phi_acc = atan2(acc[1], acc[2])*180./M_PI + 180;
-  if (phi_acc > 180.)
-  {
-    phi_acc = phi_acc - 360;
-  }
-*/
-  propagatePitchRoll(theta_rad, phi_rad, gyro_rad, dt, theta_gyr_rad, phi_gyr_rad);
+  //propagatePitchRoll(theta_rad, phi_rad, gyro_rad, dt, theta_gyr_rad, phi_gyr_rad);
   phi_acc = atan2(-1.*acc[1], sqrt(acc[1]*acc[1] + acc[2]*acc[2]))*180./M_PI;
-  //phi_gyr += (gyro[0] - phi_gyr_bias)*dt;
-  phi_gyr = phi_gyr_rad*180./M_PI;
-  //phi      = alpha*(phi + (gyro[0] - phi_gyr_bias)*dt)   + (1 - alpha)*(phi_acc);
-  phi = alpha*phi_gyr + (1 - alpha)*phi_acc;
+  phi_gyr += (gyro[0] - phi_gyr_bias)*dt + 0.5*dt*(gyro[0] - phi_prev_degps);
+  theta_gyr += (gyro[1] - theta_gyr_bias)*dt + 0.5*dt*(gyro[1] - theta_prev_degps);
+
+  //phi_gyr -= theta_gyr*sin((gyro[2] - psi_gyr_bias)*dt*M_PI/180.);
+  //theta_gyr += psi_gyr*sin((gyro[2] - psi_gyr_bias)*dt*M_PI/180.);
+  static float phi_gyr_temp = 0.;
+  static float theta_gyr_temp = 0.;
+
+  //phi_gyr_temp = phi + (gyro[0] - phi_gyr_bias)*dt + 0.5*dt*(gyro[0] - phi_gyr_bias - phi_prev_degps);
+  phi_gyr_temp = phi + (gyro[0] - phi_gyr_bias)*dt;
+  //theta_gyr_temp = theta + (gyro[1] - theta_gyr_bias)*dt + 0.5*dt*(gyro[1] - theta_gyr_bias - theta_prev_degps);
+  theta_gyr_temp = theta + (gyro[1] - theta_gyr_bias)*dt;
+
+  phi_gyr_temp -= theta_gyr_temp*sin((gyro[2] - psi_gyr_bias)*dt*M_PI/180.);
+  theta_gyr_temp += phi_gyr_temp*sin((gyro[2] - psi_gyr_bias)*dt*M_PI/180.);
   
-//  theta_acc  = atan2(-1.*acc[0], sqrt(acc[1]*acc[1] + acc[2]*acc[2]))*180./M_PI;
+  //phi_gyr = phi_gyr_rad*180./M_PI;
+  //phi      = alpha*(phi + (gyro[0] - phi_gyr_bias)*dt + 0.5*dt*(gyro[0] - phi_prev_degps))   + (1 - alpha)*(phi_acc);
+  phi      = alpha*(phi_gyr_temp)   + (1 - alpha)*(phi_acc);
+  //phi = alpha*phi_gyr + (1 - alpha)*phi_acc;
+  
   theta_acc = atan2(-1.*acc[0], acc[2])*180./M_PI + 180;
   if (theta_acc > 180.)
   {
     theta_acc -= 360.;
   }
-  //theta_gyr += (gyro[1] - theta_gyr_bias)*dt;
-  theta_gyr = theta_gyr_rad*180./M_PI;
-  //theta      = alpha*(theta + (gyro[1] - theta_gyr_bias)*dt) + (1 - alpha)*(theta_acc);
-  theta = alpha*theta_gyr + (1 - alpha)*theta_acc;
+  
+  //theta_gyr = theta_gyr_rad*180./M_PI;
+  //theta      = alpha*(theta + (gyro[1] - theta_gyr_bias)*dt + 0.5*dt*(gyro[1] - theta_prev_degps)) + (1 - alpha)*(theta_acc);
+  theta      = alpha*(theta_gyr_temp) + (1 - alpha)*(theta_acc);
+  //theta = alpha*theta_gyr + (1 - alpha)*theta_acc;
 
   psi_gyr += (gyro[2] - psi_gyr_bias)*dt;
   psi_rate = gyro[2] - psi_gyr_bias;
 
+  phi_prev_degps = gyro[0] - phi_gyr_bias;
+  theta_prev_degps = gyro[1] - theta_gyr_bias;
+
   //Serial.print(phi_acc);
   //Serial.print(" ");
-  Serial.print(phi_gyr);
-  //Serial.print(theta);
+  //Serial.print(phi_gyr);
+  Serial.print(theta);
   //Serial.print(gyro[0] - phi_gyr_bias);
   Serial.print(" ");
   //Serial.print(gyro[1] - theta_gyr_bias);
   //Serial.print(theta_acc);
-  Serial.print(theta_gyr);
-  //Serial.print(phi);
+  //Serial.print(theta_gyr);
+  Serial.print(phi);
   //Serial.print(" ");
   //Serial.print(M_PI/180.);
   //Serial.print(psi_rate);
   //Serial.print(" ");
   Serial.println("");
   
+  //Serial.println(micros() - t);
   while ((micros() - t) < 4000);
   //Serial.println(micros() - t);
 }
