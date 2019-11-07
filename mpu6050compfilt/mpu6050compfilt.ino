ea3161345f08e5fcfe671a99452a75f908350b0f
therealjtgill <jtgill@email.arizona.edu>
2019-11-07 11:15:38 -0700

    Added slow gyro propagation to mpu6050 complementary filter test *.ino file.

--- mpu6050compfilt/mpu6050compfilt.ino
+++ mpu6050compfilt/mpu6050compfilt.ino
@@ -1,9 +1,11 @@
 #include <Wire.h>
 #include <math.h>
+#include <propagateimu.hpp>
 
 void setup(void)
 {
   Wire.begin();
+  TWBR = 12;
 
   Serial.println("Is this displaying?");
   delay(800);
@@ -30,7 +32,10 @@ void setup(void)
   Serial.begin(9600);
 }
 
-void getImuData(float * acc, float * gyro)
+/////////////////////////////////////////////////
+// getImuData
+/////////////////////////////////////////////////
+void getImuData(float * acc_meas_out, float * gyro_meas_out)
 {
   unsigned int i = 0;
   Wire.beginTransmission(0x68);
@@ -42,8 +47,7 @@ void getImuData(float * acc, float * gyro)
   
   for (i = 0; i < 3; ++i)
   {
-    acc[i] = (-1.0)*((float)((Wire.read() << 8) | Wire.read()))/4096.;
-    //Serial.println(acc[i]);
+    acc_meas_out[i] = (-1.0)*((float)((Wire.read() << 8) | Wire.read()))/4096.;
   }
 
   for (i = 0; i < 2; ++i)
@@ -53,11 +57,7 @@ void getImuData(float * acc, float * gyro)
 
   for (i = 0; i < 3; ++i)
   {
-    gyro[i] = (1.0)*((float)((Wire.read() << 8) | Wire.read()))/65.5;
-    if (i == 1)
-    {
-      gyro[i] *= -1.0;
-    }
+    gyro_meas_out[i] = (float)((Wire.read() << 8) | Wire.read())/65.5;
   }
 }
 
@@ -112,9 +112,16 @@ void loop(void)
   static float psi_rate = 0.;
   //-----------------------
 
-  static float t = 0.0;
-  static float dt = 0.01;
-  static float alpha = 0.97;
+  static double t = 0.0;
+  static double dt = 0.01;
+  static float alpha = 0.9996;
+
+  static float gyro_rad[3] = {0., 0., 0.};
+  static float phi_rad = 0.;
+  static float theta_rad = 0.;
+
+  static float phi_gyr_rad = 0.;
+  static float theta_gyr_rad = 0.;
 
   static bool initialized = false;
 
@@ -130,35 +137,57 @@ void loop(void)
   dt = (micros() - t)/1e6;
   t = micros();
   getImuData(acc, gyro);
+  gyro_rad[0] = (0.7*gyro_rad[0] + 0.3*(gyro[0] - phi_gyr_bias))*M_PI/180.;
+  gyro_rad[1] = (0.7*gyro_rad[1] + 0.3*(gyro[1] - theta_gyr_bias))*M_PI/180.;
+  gyro_rad[2] = (0.7*gyro_rad[2] + 0.3*(gyro[2] - psi_gyr_bias))*M_PI/180.;
 
+  phi_rad = phi*M_PI/180.;
+  theta_rad = theta*M_PI/180.;
+/*
   phi_acc = atan2(acc[1], acc[2])*180./M_PI + 180;
   if (phi_acc > 180.)
   {
     phi_acc = phi_acc - 360;
   }
-  phi_gyr += (gyro[0] - phi_gyr_bias)*dt;
-  phi      = alpha*(phi + (gyro[0] - phi_gyr_bias)*dt)   + (1 - alpha)*(phi_acc);
+*/
+  propagatePitchRoll(theta_rad, phi_rad, gyro_rad, dt, theta_gyr_rad, phi_gyr_rad);
+  phi_acc = atan2(-1.*acc[1], sqrt(acc[1]*acc[1] + acc[2]*acc[2]))*180./M_PI;
+  //phi_gyr += (gyro[0] - phi_gyr_bias)*dt;
+  phi_gyr = phi_gyr_rad*180./M_PI;
+  //phi      = alpha*(phi + (gyro[0] - phi_gyr_bias)*dt)   + (1 - alpha)*(phi_acc);
+  phi = alpha*phi_gyr + (1 - alpha)*phi_acc;
   
-  theta_acc  = atan2(-1.*acc[0], sqrt(acc[1]*acc[1] + acc[2]*acc[2]))*180./M_PI;
-  theta_gyr += (gyro[1] - theta_gyr_bias)*dt;
-  theta      = alpha*(theta + (gyro[1] - theta_gyr_bias)*dt) + (1 - alpha)*(theta_acc);
+//  theta_acc  = atan2(-1.*acc[0], sqrt(acc[1]*acc[1] + acc[2]*acc[2]))*180./M_PI;
+  theta_acc = atan2(-1.*acc[0], acc[2])*180./M_PI + 180;
+  if (theta_acc > 180.)
+  {
+    theta_acc -= 360.;
+  }
+  //theta_gyr += (gyro[1] - theta_gyr_bias)*dt;
+  theta_gyr = theta_gyr_rad*180./M_PI;
+  //theta      = alpha*(theta + (gyro[1] - theta_gyr_bias)*dt) + (1 - alpha)*(theta_acc);
+  theta = alpha*theta_gyr + (1 - alpha)*theta_acc;
 
   psi_gyr += (gyro[2] - psi_gyr_bias)*dt;
   psi_rate = gyro[2] - psi_gyr_bias;
 
   //Serial.print(phi_acc);
   //Serial.print(" ");
-  //Serial.print(phi_gyr);
-  Serial.print(theta);
+  Serial.print(phi_gyr);
+  //Serial.print(theta);
+  //Serial.print(gyro[0] - phi_gyr_bias);
   Serial.print(" ");
-  Serial.print(phi);
+  //Serial.print(gyro[1] - theta_gyr_bias);
+  //Serial.print(theta_acc);
+  Serial.print(theta_gyr);
+  //Serial.print(phi);
   //Serial.print(" ");
   //Serial.print(M_PI/180.);
   //Serial.print(psi_rate);
-  Serial.print(" ");
+  //Serial.print(" ");
   Serial.println("");
-
-  while ((micros() - t) < 4000);
   
+  while ((micros() - t) < 4000);
+  //Serial.println(micros() - t);
 }
 
